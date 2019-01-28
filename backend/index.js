d8dcec0891eeeb0bd526c613f75599f5652d01a1
sumodh Nair <sumodh@nagarro.com>
2019-01-28 14:45:36 +0530

    first commit!

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3000;
+
+
+app.get('/play', function(req, res) {
+  let arr = [];
+  for (i = 0; i < 3; i++) {
+    let val = Math.floor(Math.random() * 5) + 1;
+    arr.push(val);
+  }
+    res.send(JSON.stringify(arr));
+});
+
+server.listen(port, function() {
+  console.log('Server listening at port %d', port);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,72 @@
+import React, { Component,Fragment } from 'react';
+import Button from './images/button.png';
+import axios from 'axios';
+import './App.css';
+const webURL  = 'http://localhost:3000';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      totalCoins : 10000,
+      hasWonText : '',
+      isRoundFree : false,
+      result:[]
+    }
+    this.startPlay = this.startPlay.bind(this);
+  }
+
+  getDataFromServer = async () =>  {
+  try {
+    let output = await axios.get(`${webURL}/play`);
+      return output.data;
+  }catch(e) {
+    console.log(`Status: Error, ${e}`);
+  }
+  }
+  checkIfEqual = (arr) =>  arr.every( v => v === arr[0] )
+  
+  async startPlay() {
+      if(!this.state.isRoundFree) {
+        this.setState({totalCoins : this.state.totalCoins-10});
+      }
+      this.setState(prevState => ({
+          isRoundFree : !prevState.isRoundFree
+      }));
+      let data = await this.getDataFromServer();
+      this.setState({result : data});
+      if(this.checkIfEqual(data)) {
+      this.setState({hasWonText : `Big Win, you've earned 20 coins to your total`, totalCoins : this.state.totalCoins+20});
+      } else {
+        this.setState({hasWonText: "No Win. Hence no coins added to your total"})
+      }
+
+  }
+
+  render() {
+    const {totalCoins, result= [], hasWonText= "",isRoundFree=false} = this.state;
+    return (
+      <div className="container">
+        <Fragment>
+          <p id="win-text">{isRoundFree ? 'Free Round' : 'Normal Round'}</p>
+          <p id="win-text">{hasWonText}</p>
+          <div id="symbol-space">
+            {result.length>0 && result.map((value, index)=>(
+               <img  key= {index} src= {require(`./images/Symbol_${value}.png`)} className = "symbol-img" alt= "test" />
+            )) }
+          </div>
+
+          <p id="bonus-text"></p>
+          <button id="play-btn" onClick = {this.startPlay}>
+            <img alt="button" src={Button}/>
+          </button>
+        </Fragment>
+      <div className="footer">
+        <p>Total Coins : {totalCoins}</p>
+      </div>
+    </div>
+    );
+  }
+}
+
+
+export default App;
